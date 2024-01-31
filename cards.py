5f1163dacbd01701c0f1cf0b85a4f24f9ad89f6b
Taufiq Noorani <work.taufiqnoorani@gmail.com>
2024-01-31 13:42:31 +0000

    Changes to be committed: 	modified:   cards.py 	modified:   specMain.py Naming conventions.

--- cards.py
+++ cards.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-def create_deck_dataframe():
+def createDeckDataframe():
     ranks = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King', 'Ace']
     suits = ['Hearts', 'Diamonds', 'Clubs', 'Spades']
     jokers = ['Joker', 'Joker', 'Joker']
@@ -10,13 +10,13 @@ def create_deck_dataframe():
     cards.extend([(joker, 'Joker') for joker in jokers])
 
     # Create a DataFrame from the list of tuples
-    deck_df = pd.DataFrame(cards, columns=['Rank', 'Suit'])
+    deckDf = pd.DataFrame(cards, columns=['Rank', 'Suit'])
 
     #TODO: need to assign unique id to each card
-    dict_cards = {}
+    dictCards = {}
     for t in cards:
-        key = len(dict_cards)
-        dict_cards[key] = t
+        key = len(dictCards)
+        dictCards[key] = t
 
-    return dict_cards
+    return dictCards
 
--- specMain.py
+++ specMain.py
@@ -1,15 +1,13 @@
 
-from cards import create_deck_dataframe
+from cards import createDeckDataframe
 from numberOfPlayers import numberOfPlayers
 
 #  Saving the values.
 num_players = numberOfPlayers()
 print(f"You've chosen {num_players} players.")
 
-showCards = create_deck_dataframe()
+showCards = createDeckDataframe()
 #print(showCards)
 for key, value in showCards.items():
     print(f"Key: {key}, Value: {value}")
 # Asking players for names.
-
-#name = input("Enter the name")
