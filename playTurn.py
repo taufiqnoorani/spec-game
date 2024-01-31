335864ae56cabdd42139086209ddc26cd203e8f9
Taufiq Noorani <work.taufiqnoorani@gmail.com>
2024-01-31 12:12:33 +0000

    Changes for naming conventions.

--- playTurn.py
+++ playTurn.py
@@ -1,13 +1,13 @@
-def player_turn(player, players, round_scores):
+def playerTurn(player, players, round_scores):
     print(f"\n{player}'s turn:")
     #display_grid()
 
     # Take input from the player for their call
-    user_input = input("Make your call (e.g., '5,5 Q,H' for row 5, column 5, and guess Queen of Hearts): ")
+    userInput = input("Make your call (e.g., '5,5 Q,H' for row 5, column 5, and guess Queen of Hearts): ")
 
     # Split the input into row, column, and guess
     try:
-        row, col, guess = map(str.strip, user_input.split())
+        row, col, guess = map(str.strip, userInput.split())
         row, col = int(row), int(col)
     except ValueError:
         print("Invalid input format. Please try again.")
