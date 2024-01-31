3cc1400e701c274752f3a1aff57fc39eb4291cc9
saleh <soleh5617@gmail.com>
2024-01-31 00:53:28 +0000

    31stjan

--- numberOfPlayers.py
+++ numberOfPlayers.py
@@ -11,10 +11,3 @@ def numberOfPlayers():
 
     return players
 
-#  Saving the values.
-num_players = numberOfPlayers()
-print(f"You've chosen {num_players} players.")
-
-# Asking players for names.
-
-name = input("Enter the name")
---
+++ specMain.py
@@ -0,0 +1,9 @@
+from numberOfPlayers import numberOfPlayers
+
+#  Saving the values.
+num_players = numberOfPlayers()
+print(f"You've chosen {num_players} players.")
+
+# Asking players for names.
+
+#name = input("Enter the name")
