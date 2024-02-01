f2b7df163389c0d49be926f7435d62905e218bdb
Taufiq Noorani <work.taufiqnoorani@gmail.com>
2024-02-01 13:44:29 +0000

    Changes to be committed: modified:   numberOfPlayers.py modified:   setupRound.py
    For numberOfPlayers.py, the function now returns
    the number of players and their names.
    
    For setupRound.py, the only spacing changes, no changes were made
    to the logic.

--- numberOfPlayers.py
+++ numberOfPlayers.py
@@ -1,18 +1,22 @@
 def numberOfPlayers():
     while True:
         try:
-            players = int(
-                input("Enter the number of players playing (must be between 2 to 4): "))
+            players = int(input("Enter the number of players playing (must be between 2 to 4): "))
             if 2 <= players <= 4:
                 break  # Break out of the loop if a valid number of players is entered
             else:
                 print("Invalid player number. Please choose a value between 2 and 4.")
         except ValueError:
             print("Invalid input. Please enter a valid integer between 2 and 4.")
+
     # Asking for player names.
     names = []
     for i in range(players):
         name = input(f"Enter name for Player {i + 1}: ")
         names.append(name)
 
-    return names
+    return players, names
+
+
+playerNumber, playerName = numberOfPlayers()
+print(f"The players are: {playerName}.")
--- setupRound.py
+++ setupRound.py
@@ -1,9 +1,7 @@
 import pandas as pd
 
 scorecards = pd.read_csv("scorecards.csv")
-
 print(scorecards)
 
 showcards = pd.read_csv("showcards.csv")
-
 print(showcards)
