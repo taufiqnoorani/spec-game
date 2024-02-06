f88adf5a771a66fc5f27dba7e57c7848e41eb1b7
MananT4 <thakkar.manan@yahoo.co.in>
2024-02-06 12:51:08 +0000

    Updated get methods to return list of list

--- setupRound.py
+++ setupRound.py
@@ -2,8 +2,8 @@ import pandas as pd
 import random
 
 #My file path is different for some reason, use which ever works for you
-filePath = "spec-game/"
-#filePath = ""
+#filePath = "spec-game/"
+filePath = ""
 
 #The main file should have this globally declared in the future
 #For displaying in the terminal, might be cleaner to show the symbol instead of the text for suites
@@ -14,13 +14,40 @@ suiteDict = {"Spade": "♠",
              "Joker": "🃏"}
 
 
+#It is still using a CSV to fetch only.
+#This can be changed to be statically typed in the future, if there any more changes to the format of the data, CSV is easier to edit
+#RETURNS a list of list containing all the scorecards
+#[[Points, Rank, Suite], [Points, Rank, Suite], ...]
 def getScorecards():
+    scorecardList = []
     scorecards = pd.read_csv(filePath+"scorecards.csv")
-    return scorecards
+    for index, row in scorecards.iterrows():
+        sc = [str(row["Points"]),str(row["Rank"]),str(row["Suite"])]
+        scorecardList.append(sc) 
+    return scorecardList
 
+
+
+##############  USE THIS TO GET A GRID LIST   ##############
+#Returns a randomized list of list containing all the showcards
+#[[1, Rank, Suite], [2, Rank, Suite], [3, Rank, Suite] ... [25, Rank, Suite]]
 def getShowcards():
+    showcardList = []
+    randomList = random.sample(range(0,25),25)
     showcards = pd.read_csv(filePath+"showcards.csv")
-    return showcards
+    i = 1
+    for r in randomList:
+        sc = [str(i), str(showcards.iloc[r].Rank), str(showcards.iloc[r].Suite)]
+        showcardList.append(sc)
+        i+=1
+    return showcardList
+
+
+
+
+###########################################################################
+#I am leaving this method for now. Use getShowcards() to get the gird list#
+###########################################################################
 
 #The Grid is a 5x5 dataframe.
 #Each cell is a list containing [Rank,Suit] as a list. eg - [Jack,Heart]
@@ -44,5 +71,3 @@ def generateGrid(showcards):
     #print(grid)
     grid.to_csv(filePath+"grid.csv")
             
-
-generateGrid(getShowcards())
