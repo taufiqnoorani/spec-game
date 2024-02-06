bba7ac22c4932c576e19b398669790e0f3665150
Taufiq Noorani <work.taufiqnoorani@gmail.com>
2024-02-06 21:16:14 +0000

    modified:   playTurn.py for scoring.

--- playTurn.py
+++ playTurn.py
@@ -55,15 +55,15 @@ def checkGuess(updatedGrid, guess):
     score = 0
     for card in updatedGrid:
         if card[0] == guess[0]:  # If the position matches
-            if card[1] == guess[1] and card[2] == guess[2]:  # If the suit and rank match
+            if card[1] == guess[1] and card[2] == guess[2]:  # If the rank and suit match
                 score = 4  # Exact card guessed
-            elif card[1] == guess[1]:  # If only the suit matches
-                score = 1  # Card of the same suit
-                if card[2] == "JOKER":  # If the card is a Joker of the suit called
-                    score = 2  # Joker of the suit called
-            elif card[2] == guess[2]:  # If only the rank matches
+            elif card[1] == guess[1]:  # If only the rank matches
                 score = 2  # Card of the same rank
-            if card[2] == "JOKER" and card[1] == "JOKER":  # If the card is the real Joker
+            elif card[2] == guess[2]:  # If only the suit matches
+                score = 1  # Card of the same suit
+                if card[1] == "JOKER":  # If the card is a Joker of the suit called
+                    score = 2  # Joker of the suit called
+            if card[1] == "JOKER" and card[2] == "JOKER":  # If the card is the real Joker
                 score = 4  # Real Joker guessed
             break  # Exit the loop as the card has been found
     return score  # Return the score
