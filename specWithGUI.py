2bb13b2e9e5e5da27c7a8f2fb2ccc999cc7bbb04
Taufiq Noorani <work.taufiqnoorani@gmail.com>
2024-03-12 13:19:23 +0000

    Changes to be committed: modified:   specWithGUI.py Defining the main structure of the game. Will use this as a base for the rest of the game. Next step is to fill up the functions with the logic (which is already defined) but change it to GUI related. Last step will be to fill the main loop function to order the game correctly.

--- specWithGUI.py
+++ specWithGUI.py
@@ -1,11 +1,97 @@
+import random
 import tkinter as tk
 import tkinter.ttk as ttk
 from tkinter import messagebox
 
 
+# Framing.
 window = tk.Tk()
 window.title("Spec")
 window.geometry('1280x800')
 
 frame = ttk.Frame(window)
 frame.place(relx=0.5, rely=0.5, anchor='center')
+
+
+# Setting up cards and grid.
+def scorecards():
+   # Create scorecards
+   return scorecards
+def showcards():
+   # Create showcards.
+   return showcards
+def displayGrid():
+   # Generate grid.
+   return displayGrid
+
+
+# Global Variables.
+numberOfPlayers = 0
+gameScores = {}
+names = []
+predictions = {}
+
+
+# Game Logic.
+def numberOfPlayer():
+   # Get number of players.
+   return numberOfPlayers
+
+
+def nameOfPlayer():
+   # Get names of players.
+   return names
+
+
+def randomNames():
+   # Randomize the names.
+   return names
+
+
+def preRoundPredictions():
+   # Ask for pre-round predictions and save it.
+   return predictions
+
+
+def guess():
+   # Guess logic.
+   return guess
+
+
+def updateGrid():
+   # Update the grid.
+   return updateGrid
+
+
+def analyzeGrid():
+   # Analyze the grid.
+   return analyzeGrid
+
+
+def validateCallout():
+   # Validate the callout.
+   return validateCallout
+
+
+def preRoundScoring():
+   # Pre-round scoring.
+   return preRoundScoring
+
+
+def endRoundScoring():
+   # End-round scoring.
+   return endRoundScoring
+
+
+def endRoundCondition():
+   # End-round condition.
+   return endRoundCondition   
+
+
+def endRoundLast():
+   # End-round last.
+   return endRoundLast
+
+
+# Initialization of the game a.k.a. the specMain section.
+window.mainloop()
