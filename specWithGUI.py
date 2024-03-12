a67e98e0a8769f43fbe5766a79b12ae636e8edea
Taufiq Noorani <work.taufiqnoorani@gmail.com>
2024-03-12 11:42:20 +0000

    Starting with GUI base.

---
+++ specWithGUI.py
@@ -0,0 +1,11 @@
+import tkinter as tk
+import tkinter.ttk as ttk
+from tkinter import messagebox
+
+
+window = tk.Tk()
+window.title("Spec")
+window.geometry('1280x800')
+
+frame = ttk.Frame(window)
+frame.place(relx=0.5, rely=0.5, anchor='center')
